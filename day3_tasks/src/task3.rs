65bdcf9995822a249c6c583415fd32d46c400ac0
Gris Ge <fge@redhat.com>
2023-10-13 11:01:02 +0800

    Add code for task3
    Signed-off-by: Gris Ge <fge@redhat.com>

--- day3_tasks/src/task3.rs
+++ day3_tasks/src/task3.rs
@@ -13,21 +13,23 @@ fn merged_hash(
     ret
 }
 
+fn is_subnet_nested(
+    subset: &HashMap<String, HashMap<String, String>>,
+    super_set: &HashMap<String, HashMap<String, String>>,
+) -> bool {
+    subset.iter().all(|(k, v)| {
+        super_set
+            .get(k)
+            .map(|sup_v| is_subnet(v, sup_v))
+            .unwrap_or_default()
+    })
+}
+
 fn is_subnet(
     subset: &HashMap<String, String>,
     super_set: &HashMap<String, String>,
 ) -> bool {
     subset.iter().all(|(k, v)| super_set.get(k) == Some(v))
-    for (k, v) in subset {
-        if let Some(sup_v) = super_set.get(k) {
-            if sup_v != v {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    }
-    true
 }
 
 fn main() {
@@ -41,6 +43,19 @@ fn main() {
     let new_hash = merged_hash(&src, &dst);
     println!("HAHA {:?}", new_hash);
 
-    println!("src is subset of new: {}", is_subnet(&src, &new_hash));
-    println!("dst is subset of new: {}", is_subnet(&dst, &new_hash));
+    let mut sub_src = HashMap::new();
+    sub_src.insert("section_a".to_string(), src);
+    let mut sub_dst = HashMap::new();
+    sub_dst.insert("section_a".to_string(), dst);
+    let mut sup_h = HashMap::new();
+    sup_h.insert("section_a".to_string(), new_hash);
+
+    println!(
+        "src is subset of new: {}",
+        is_subnet_nested(&sub_src, &sup_h)
+    );
+    println!(
+        "dst is subset of new: {}",
+        is_subnet_nested(&sub_dst, &sup_h)
+    );
 }
